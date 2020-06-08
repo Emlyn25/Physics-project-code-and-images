9318ca80e2cf696ef0f82574789ca4e9fb8dff78
Emlyn25 <66638666+Emlyn25@users.noreply.github.com>
2020-06-08 20:35:30 +0100

    question 6, 7 and 8 all in one
    shows data path and prediction based on equation from Q3

--- Q7_Q8.py
+++ Q7_Q8.py
@@ -1,18 +1,31 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 File = open("Galaxy1.txt","r")
 next(File)
 x=[]
 y=[]
-p=100*10**6
+m=[]
 for i in File.readlines():
     x.append(float(i.split()[0]))
     y.append(float(i.split()[1]))
+    m.append(float(i.split()[4]))
 x = np.array(x)
 y = np.array(y)
-x2 = np.arange(max(x))
-y2 = x2*np.sqrt((4/3)*(4.3*10**-6)*(p)*np.pi)
-plt.plot(x,y,"ro")
+
+y2=[]
+x2 = list(range(1, int(max(x))))
+for i in x2:
+    if i==0: next
+    else:
+        print(m[x2.index(i)])
+        print(i)
+        y2.append(math.sqrt((4.3*10**-6) * m[x2.index(i)] * 1/i))
+
+x2=np.array(x2)
+        
+y2 = np.array(y2)
+plt.plot(x,y)
 plt.plot(x2,y2)
 plt.xlabel("Radius (kpc)")
 plt.ylabel("Velocity (kms^-1)")
